397866c76563bb4d50e24db5ada15d3ea7252885
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2022-10-09 23:25:13 -0400

    3.3 comments

--- 3.3Comments/main.cpp
+++ 3.3Comments/main.cpp
@@ -1,6 +1,29 @@
+// This brings in the iostream library
 #include <iostream>
 
+/*
+    This is a block comment that englobes mutiple 
+    lines of text
+*/
+
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    // This is going to print "Hello World" to the console
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    // Program ends here
 }
+
+
+
+// Comments out a single ine
+
+/* ... */ //Block Comments out a block of text
+
+/*
+    Block comments
+    can't be
+    nested
+    It breaks the code 
+*/
+
+// Use Comments to document your code. Don't overdo it though.
