3bf49108433702b8a73ec87d5f27870450b8460b
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2022-10-14 18:23:44 -0400

    12.5 Bounds of An Array

--- 12.5BoundsOfAnArray/main.cpp
+++ 12.5BoundsOfAnArray/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+
+    // Array bounds
+    /*
+    int numbers [] {1,2,3,4,5,6,7,8,9,0};
+
+    // Read beyond bounds : Will read garbage or crash your program
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
+
+    // Write beyond bound. The complier allow it. But you don't own
+    // the memory at index 12, so other programs may modify it and your
+    // program may read bogus data at a later time. Or you can evem
+    // corrupt data used by other parts of your program
+    numbers[12] = 1000;
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
+    */
+   // ===================================================================
+
+    return 0;
 }
+
+
+// Bounds Of an array
+
+    // Array bounds
+    /*
+    int numbers [] {1,2,3,4,5,6,7,8,9,0};
+
+    // Read beyond bounds : Will read garbage or crash your program
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
+
+    // Write beyond bound. The complier allow it. But you don't own
+    // the memory at index 12, so other programs may modify it and your
+    // program may read bogus data at a later time. Or you can evem
+    // corrupt data used by other parts of your program
+    numbers[12] = 1000;
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
--- README.md
+++ README.md
@@ -609,3 +609,23 @@ Three different ways
 
 
 12.4 Arrays of Character 9:26:45
+
+// Bounds Of an array
+
+    // Array bounds
+    /*
+    int numbers [] {1,2,3,4,5,6,7,8,9,0};
+
+    // Read beyond bounds : Will read garbage or crash your program
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
+
+    // Write beyond bound. The complier allow it. But you don't own
+    // the memory at index 12, so other programs may modify it and your
+    // program may read bogus data at a later time. Or you can evem
+    // corrupt data used by other parts of your program
+    numbers[12] = 1000;
+    std::cout << "numbers[12] : " << numbers[12] << std::endl;
+
+
+Pointer 9:53:31
+
